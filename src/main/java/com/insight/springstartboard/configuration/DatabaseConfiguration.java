8932cee18be60101a7a18f8acb2078a93ba385ec
adorno <pch4623@gmail.com>
2019-08-03 17:06:56 +0900

    마이바티스 연동

--- src/main/java/com/insight/springstartboard/configuration/DatabaseConfiguration.java
+++ src/main/java/com/insight/springstartboard/configuration/DatabaseConfiguration.java
@@ -1,6 +1,11 @@
 package com.insight.springstartboard.configuration;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -14,6 +19,8 @@ import javax.sql.DataSource;
 @Configuration
 @PropertySource("classpath:/application.properties")    // 설정파일 위치 지정
 public class DatabaseConfiguration {
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")   // 데이터베이스 관련 정보를 사용하도록 지정
@@ -28,5 +35,19 @@ public class DatabaseConfiguration {
         System.out.println(dataSource.toString());
         System.out.println("=========================");
         return dataSource;
+        //return new HikariDataSource(hikariConfig());
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/sql-*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
     }
 }
--- src/test/java/com/insight/springstartboard/SpringStartBoardApplicationTests.java
+++ src/test/java/com/insight/springstartboard/SpringStartBoardApplicationTests.java
@@ -2,6 +2,8 @@ package com.insight.springstartboard;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringStartBoardApplicationTests {
 
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testSqlSession() throws Exception {
+		System.out.println("\n>>>>>>>>>>>>>>>>>");
+		System.out.println(sqlSession.toString());
+		System.out.println(">>>>>>>>>>>>>>>>>");
 	}
 
 }
