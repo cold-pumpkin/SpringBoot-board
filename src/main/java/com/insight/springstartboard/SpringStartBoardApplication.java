6e20ffc1e435184dd10317e1b892573a671a01c6
adorno <pch4623@gmail.com>
2019-08-03 13:51:05 +0900

    의존성 추가

---
+++ src/main/java/com/insight/springstartboard/SpringStartBoardApplication.java
@@ -0,0 +1,13 @@
+package com.insight.springstartboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartBoardApplication.class, args);
+	}
+
+}
