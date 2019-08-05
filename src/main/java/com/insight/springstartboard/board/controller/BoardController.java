407d497ef58ef1e20e06a8821aac0d8c6381de56
adorno <pch4623@gmail.com>
2019-08-05 22:48:21 +0900

    게시글 등록 기능 구현

--- src/main/java/com/insight/springstartboard/board/controller/BoardController.java
+++ src/main/java/com/insight/springstartboard/board/controller/BoardController.java
@@ -15,6 +15,7 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
+    // 게시글 목록 조회
     @RequestMapping("/board/openBoardList.do")
     public ModelAndView openBoardList() throws Exception {
         ModelAndView mv = new ModelAndView("/board/boardList"); // templates/board/boardList.html
@@ -23,5 +24,18 @@ public class BoardController {
         mv.addObject("list", list); // 비즈니스 로직 결과 값을 뷰에 list 라는 이름으로 저장
 
         return mv;
+    }
+
+    // 게시글 작성 화면 호출
+    @RequestMapping("/board/openBoardWriter.do")
+    public String openBoardWriter() throws Exception {
+        return "/board/boardWrite";
+    }
+
+    // 작성된 게시글 등록
+    @RequestMapping("/board/insertBoard.do")
+    public String insertBoard(BoardDto board) throws Exception {
+        boardService.insertBoard(board);
+        return "redirect:/board/openBoardList.do";  // 게시글 작성 후 게시글 목록 화면으로 이동
     }
 }
--- src/main/java/com/insight/springstartboard/board/service/BoardService.java
+++ src/main/java/com/insight/springstartboard/board/service/BoardService.java
@@ -5,5 +5,9 @@ import com.insight.springstartboard.board.dto.BoardDto;
 import java.util.List;
 
 public interface BoardService {
+
     List<BoardDto> selectBoardList() throws Exception;
+
+    void insertBoard(BoardDto board) throws Exception;
 }
+
