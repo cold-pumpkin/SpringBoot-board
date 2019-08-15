705769e76e082b1363001d035cd81a26b1eabed9
adorno <pch4623@gmail.com>
2019-08-15 17:09:01 +0900

    트랙잭션 처리, 예외처리, 한글 인코딩 처리

--- src/main/java/board/BoardApplication.java
+++ src/main/java/board/BoardApplication.java
@@ -1,13 +1,13 @@
-package com.insight.springstartboard;
+package board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringStartBoardApplication {
+public class BoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringStartBoardApplication.class, args);
+		SpringApplication.run(BoardApplication.class, args);
 	}
 
 }
---
+++ src/main/java/board/board/service/BoardServiceImpl.java
@@ -0,0 +1,46 @@
+package board.board.service;
+
+import board.board.dto.BoardDto;
+import board.board.mapper.BoardMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class BoardServiceImpl implements BoardService{
+	
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	@Override
+	public List<BoardDto> selectBoardList() throws Exception {
+		return boardMapper.selectBoardList();
+	}
+	
+	@Override
+	public void insertBoard(BoardDto board) throws Exception {
+		boardMapper.insertBoard(board);
+	}
+
+	@Override
+	public BoardDto selectBoardDetail(int boardIdx) throws Exception{
+		BoardDto board = boardMapper.selectBoardDetail(boardIdx);
+		boardMapper.updateHitCount(boardIdx);
+		//int i = 10 / 0;	-- Transaction test
+		return board;
+	}
+	
+	@Override
+	public void updateBoard(BoardDto board) throws Exception {
+		boardMapper.updateBoard(board);
+	}
+
+	@Override
+	public void deleteBoard(int boardIdx) throws Exception {
+		boardMapper.deleteBoard(boardIdx);
+	}
+}	
+
