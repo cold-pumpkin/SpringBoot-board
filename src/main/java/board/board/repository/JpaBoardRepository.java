ecfc5efb6f2a91aede6237d562f65eebf23784a7
adorno <pch4623@gmail.com>
2019-11-07 23:32:12 +0900

    JpaBoardRepository 수정

--- src/main/java/board/board/repository/JpaBoardRepository.java
+++ src/main/java/board/board/repository/JpaBoardRepository.java
@@ -9,8 +9,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface JpaBoardRepository extends CrudRepository<BoardEntity, Integer> {
+    // CrudRepository 인터페이스가 제공하는 메서드
     List<BoardEntity> findAllByOrderByBoardIdxDesc();
 
+    // 쿼리 메서드
     @Query("SELECT file FROM BoardFileEntity file WHERE board_idx = :boardIdx AND idx = :idx")
     BoardFileEntity findBoardFile(@Param("boardIdx") int boardIdx, @Param("idx") int idx);
 }
